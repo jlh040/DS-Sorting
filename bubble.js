4a8d9619473694a622e6ec05cc8ee32759e10603
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-15 11:47:09 -0700

    implemented the bubble sort function

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+function bubbleSort(_arr) {
+  for (let i = 0; i < _arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < _arr.length - i; j++) {
+      if (_arr[j + 1] < _arr[j]) {
+        let temp = _arr[j];
+        _arr[j] = _arr[j + 1];
+        _arr[j + 1] = temp;
+        swapped = true
+      }
+    }
+    if (!swapped) return _arr;
+  }
+
+  return _arr;
+}
 
 module.exports = bubbleSort;
